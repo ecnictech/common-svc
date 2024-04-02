e0f14657585ad351de786144215923ebd938d61e
Adex <aeitiao@gmail.com>
2024-04-02 11:54:29 +0700

    initial commit

---
+++ src/main/java/ecnic/commonsvc/CommonSvcApplication.java
@@ -0,0 +1,13 @@
+package ecnic.commonsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ecnic/commonsvc/CommonSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ecnic.commonsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
